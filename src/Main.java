c9ed0e5d5de1f65f180058a167ad14906283f113
Sapryha Keterina <amira.alima@gmail.com>
2013-09-16 22:29:50 +0300

    final version

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,30 @@
-package stek;
-import java.util.Stack;
-import java.util.EmptyStackException; 
-class StackExample
-{
- static void showpush(Stack st, int a)
- { 
-  st.push(new Integer(a));
-  System.out.println("push(" + a + ")");
-  System.out.println("stack: " + st);
- }
- static void showpop(Stack st)
- { 
-  System.out.print("pop -> ");
-  Integer a = (Integer) st.pop();
-  System.out.println(a);
-  System.out.println("stack: " + st);
- }
- public static void main(String args[])
- { 
-  Stack st = new Stack();
-  System.out.println("stack: " + st);
-  showpush(st, 42);
-  showpush(st, 66);
-  showpush(st, 99);
-  showpop(st);
-  showpop(st);
-  showpop(st);
-  try
-  {
-   showpop(st);
-  } 
-  catch (EmptyStackException e)
-  { 
-   System.out.println("empty stack");
-  }
- }
+public class Stack {
+
+    int size = 0;
+    int[] data;
+         
+    Stack(int capacity) {
+        data = new int [capacity];
+    }
+         
+    void push(int value) {
+        try {
+            data[size++] = value;
+        }
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+         
+    int pop() {
+        return data[--size];
+    }
+    
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        for (int i = 0; i < 6; i++)
+            stack.push(i);
+        for (int i = 0; i < 5; i++)
+            stack.pop();
+    }
 }
